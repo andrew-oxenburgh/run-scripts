db8416da0227d37548ef78830fba3ac2a2d8e31c
Andrew <andrew.oxenburgh@gmail.com>
2023-05-14 11:52:58 +1200

    yarn 2 now

--- src/handleOpts.ts
+++ src/handleOpts.ts
@@ -9,21 +9,12 @@ module.exports = {
         program.command('menu')
             .description('show all scripts available')
             .action(()=>{
-                require('./showScripts.ts')
+                require('./showScripts.ts').showScripts()
             })
-            // .argument('<string>', 'string to split')
-            // .option('--first', 'display just the first substring')
-            // .option('-s, --separator <char>', 'separator character', ',')
-            // .action((str, options) => {
-            //     const limit = options.first ? 1 : undefined;
-            //     console.log(str.split(options.separator, limit));
-            // });
 
         program.parse();
 
-        const options = program.opts();
-        const limit = options.first ? 1 : undefined;
-        return options
+        return program.opts()
     }
 }
 
--- src/showScripts.ts
+++ src/showScripts.ts
@@ -1,6 +1,26 @@
 const fs = require('fs');
-const YAML = require('yaml');
+const term = require('terminal-kit').terminal;
+const R = require('ramda')
+module.exports = {
+    showScripts: () => {
+        const scripts = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+        const table = R.toPairs(scripts.scripts)
+        term.table([
+                ['name', 'command'],
+                ...table
+            ], {
+                hasBorder: true,
+                contentHasMarkup: true,
+                borderChars: 'lightRounded',
+                borderAttr: {color: 'blue'},
+                textAttr: {bgColor: 'default'},
+                // firstCellTextAttr: {bgColor: 'blue'},
+                firstRowTextAttr: {bgColor: 'white', color: 'black', bold: true},
+                // firstColumnTextAttr: {bgColor: 'red'},
+                expandToWidth: true,
+                fit: true   // Activate all expand/shrink + wordWrap
+            }
+        );
+    }
+}
 
-const obj = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-
-console.log(YAML.stringify(obj.scripts, null, 3))
